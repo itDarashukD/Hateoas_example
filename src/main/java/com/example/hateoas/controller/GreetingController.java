69a31dc0b50303598c25c3fed22f75fbadafb79b
Dzmitry_Darashuk <itDarashukD@gmail.com>
2022-02-24 10:13:00 +0300

    with linkTo and methodOn - statics methods

---
+++ src/main/java/com/example/hateoas/controller/GreetingController.java
@@ -0,0 +1,30 @@
+package com.example.hateoas.controller;
+
+import com.example.hateoas.entity.Greeting;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
+
+@RestController
+public class GreetingController {
+
+    private static final String TEMPLATE = "Hello, %s!";
+
+    @GetMapping("/greeting")
+    public HttpEntity<Greeting> greeting(@RequestParam(value = "name", defaultValue = "world") String name) {
+
+        Greeting greeting = new Greeting(String.format(TEMPLATE, name));
+        greeting.add(linkTo(methodOn(GreetingController.class).greeting(name)).withSelfRel());
+
+        return new ResponseEntity<>(greeting,HttpStatus.OK);
+
+    }
+
+
+}
