c453893b5a3bc61602055bdd4bd608a5edb647df
Dzmitry_Darashuk <itDarashukD@gmail.com>
2022-02-24 12:27:51 +0300

    Resources<Resource<GratingWithResources>

--- src/main/java/com/example/hateoas/controller/GreetingController.java
+++ src/main/java/com/example/hateoas/controller/GreetingController.java
@@ -1,6 +1,8 @@
 package com.example.hateoas.controller;
 
-import com.example.hateoas.entity.Greeting;
+import com.example.hateoas.entity.GratingWithResources;
+
+import org.springframework.hateoas.Link;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -8,8 +10,13 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
-import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
+import java.util.List;
+
+import static org.springframework.hateoas.mvc.ControllerLinkBuilder.linkTo;
+import static org.springframework.hateoas.mvc.ControllerLinkBuilder.methodOn;
+
+//import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
+//import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
 
 @RestController
 public class GreetingController {
@@ -17,12 +24,13 @@ public class GreetingController {
     private static final String TEMPLATE = "Hello, %s!";
 
     @GetMapping("/greeting")
-    public HttpEntity<Greeting> greeting(@RequestParam(value = "name", defaultValue = "world") String name) {
+    public HttpEntity<GratingWithResources.UserGreetings> greeting(
+            @RequestParam(value = "name", defaultValue = "world") String name) {
 
-        Greeting greeting = new Greeting(String.format(TEMPLATE, name));
-        greeting.add(linkTo(methodOn(GreetingController.class).greeting(name)).withSelfRel());
+        GratingWithResources.UserGreetings resources = new GratingWithResources.UserGreetings();
+        resources.add(linkTo(methodOn(GreetingController.class).greeting(name)).withSelfRel());
 
-        return new ResponseEntity<>(greeting,HttpStatus.OK);
+        return new ResponseEntity<>(resources, HttpStatus.OK);
 
     }
 
--- src/main/java/com/example/hateoas/entity/Greeting.java
+++ src/main/java/com/example/hateoas/entity/Greeting.java
@@ -1,17 +1,17 @@
-package com.example.hateoas.entity;
-
-import com.fasterxml.jackson.annotation.JsonCreator;
-import com.fasterxml.jackson.annotation.JsonProperty;
-import lombok.Data;
-import org.springframework.hateoas.RepresentationModel;
-
-@Data
-public class Greeting extends RepresentationModel<Greeting> {
-
-    private final String content;
-
-    @JsonCreator
-    public Greeting(@JsonProperty("content") String content) {
-        this.content = content;
-    }
-}
+//package com.example.hateoas.entity;
+//
+//import com.fasterxml.jackson.annotation.JsonCreator;
+//import com.fasterxml.jackson.annotation.JsonProperty;
+//import lombok.Data;
+//import org.springframework.hateoas.RepresentationModel;
+//
+//@Data
+//public class Greeting extends RepresentationModel<Greeting> {
+//
+//    private final String content;
+//
+//    @JsonCreator
+//    public Greeting(@JsonProperty("content") String content) {
+//        this.content = content;
+//    }
+//}
